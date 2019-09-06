84839e36f142d67cf905eb5c138b46f6434b0e24
Prabhakaran Srinivasan <prabhakarans.dev@gmail.com>
2019-09-06 12:12:02 +0200

    initial commit

---
+++ coinbuyalert.py
@@ -0,0 +1,49 @@
+#author	 :	PrabhakaranS
+#purpose :	watches the crypto market and alerts when its time to buy
+import requests
+import json
+from  collections import OrderedDict
+import misound
+from time import sleep
+import proxy
+
+def coinwatch():
+	f = open('buy.properties','r')
+	pf = f.read()
+	params = json.loads(pf,object_pairs_hook=OrderedDict)
+	coins = params['coin']
+	hval = 0
+	wval = 0
+	printdata = ''
+	
+	for k,v in coins.items():
+		try :
+			for ki,vi in v.items():
+				url = 'https://api.bitfinex.com/v1/pubticker/'+ki+'usd'
+				response = requests.request("GET", url)
+				data =  json.loads(response.text)
+				if (k == 'have'):
+					hval = round(float(vi) * float(data['ask']),2)
+					printdata = ki +': '+vi+' * '+data['ask'] + '='+str(hval)+' <> '
+				else :
+					wval = round(hval/float(data['bid']),2)
+					printdata += ki +': '+vi+' * '+data['bid'] + '='+str(wval)+' <> '
+					if wval >= float(vi):
+						print('buy now.')
+						misound.play()
+				
+		except Exception as e:
+			print('cant reach the network: ' +str(e))
+	print('hval: '+str(hval) + ' wval: '+str(round(wval,2)))
+	print(printdata)
+while True:
+	coinwatch()
+	print('**********************')
+	sleep(60)
+# {
+# "coin": {
+# "have" : {"xlm": "35000"},
+# "want" : {"btg": "203"},
+# "want1" : {"etp": "3264"}
+# }
+# }
