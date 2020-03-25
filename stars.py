f3dbb1b0f528a6f58514f7f1320acaf48cd5f2a4
Prabhakaran Srinivasan <prabhakarans.dev@gmail.com>
2020-03-25 11:10:18 +0200

    Update stars.py

--- stars.py
+++ stars.py
@@ -1,5 +1,13 @@
-#sulime text plugin to underline words with stars
+#sublime create plugins:
+#***********************
+#https://www.sublimetext.com/docs/plugin-examples
+#
+#working plugin
+#**************
+#create a new plugin using Tools-->Developer--> New Plugin and add the below code. Then create the key binding to create a shortcut to call the new plugin.
+
 import sublime, sublime_plugin
+#plugin to underline words with stars
 
 class StarsCommand(sublime_plugin.TextCommand):
     def run(self, edit):
@@ -16,3 +24,9 @@ class StarsCommand(sublime_plugin.TextCommand):
                 s = s+'\n'+stars
                 #replace the selection with transformed text
                 self.view.replace(edit,region, s)
+
+#key_binding:
+#************
+[
+	{ "keys": ["ctrl+8"], "command": "stars" }
+]
